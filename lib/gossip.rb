0a153ff3db38f11f9ac21847322c7ee4ac0340f1
Djo-75 <j.pecquet@live.fr>
2023-02-06 16:37:24 +0100

    2.5_inclus

---
+++ lib/gossip.rb
@@ -0,0 +1,32 @@
+# require 'pry'
+require 'csv'
+
+class Gossip
+
+    attr_accessor :author, :content
+
+    def initialize(author, content)
+      @author = author
+      @content = content
+    end
+
+    def save # méthode d'instance
+        CSV.open("./db/gossip.csv", "a+") do |csv|
+          csv << [@author, @content]
+        end
+        rescue => error
+        puts "Error while writing to the CSV file: #{error}"
+    end
+
+    def self.all
+      all_gossips = []
+      CSV.read("./db/gossip.csv").each do |csv_line|
+        all_gossips << Gossip.new(csv_line[0], csv_line[1])
+      end
+      return all_gossips
+    end    
+end
+
+# binding.pry
+
+# Gossip.new("José", "Josiane est nulle").save
